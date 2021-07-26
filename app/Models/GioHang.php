087d1331fd406b8aca6b11c9df78e784d462adad
Quyet Ngo <nguynhuan123@gmail.com>
2021-07-26 19:36:52 +0700

    Update GioHang.php

--- app/Models/GioHang.php
+++ app/Models/GioHang.php
@@ -96,10 +96,10 @@ class GioHang extends Model
 
 	//xóa cart
 	public function removeItem($id) {
-		// $this->tongSL -= $this->items[$id]['so_luong'];
-		// $this->tongTien -= $this->items[$id]['gia'];
-		$this->tongSL = 0;
-		$this->tongTien = 0;
+		$this->tongSL -= $this->items[$id]['so_luong'];
+		$this->tongTien -= $this->items[$id]['gia'];
+		// $this->tongSL = 0;
+		// $this->tongTien = 0;
 		unset($this->items[$id]);
 	}
 
